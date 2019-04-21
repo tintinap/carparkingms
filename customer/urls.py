ff76bd7ee1832ca53084c30db46b7c15464266a7
Supakit Theanthunyakit <mr.pokiloli@gmail.com>
2019-04-21 13:49:19 +0700

    Route Page

--- customer/urls.py
+++ customer/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from customer import views
 
 urlpatterns = [
-    path('', views.index),
-    path('register/', views.register),
-    path('login/', views.my_login),
-    path('buy_package/', views.buypackage),
-    path('profile/', views.profile),
-    path('edit_profile/', views.editprofile),
-    path('add_car/', views.addcar),
-    path('change_password/', views.changepassword),
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.my_login, name='my_login'),
+    path('buy_package/', views.buypackage, name='buy_package'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile/', views.editprofile, name='edit_profile'),
+    path('add_car/', views.addcar, name='add_car'),
+    path('change_password/', views.changepassword, name='change_password'),
 ]
